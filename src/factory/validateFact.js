20530536a05deb9d6530a484e1e71e44c27cbd5c
Sasa M <smikodanic@gmail.com>
2017-01-19 12:44:32 +0100

    fix

--- src/factory/validateFact.js
+++ src/factory/validateFact.js
@@ -66,7 +66,7 @@ module.exports = function () {
 
             number: function (inputModel, iElem, scope, iAttrs) {
 
-                //TODO CORRECTOR: converting model's value to number when <input type="text"> is used
+                //ToDo CORRECTOR: converting model's value to number when <input type="text"> is used
                 if (iElem.attr('type') !== 'number') iElem.attr('type', 'number');
                 var newVal = Number(inputModel) || inputModel;
 
@@ -236,14 +236,19 @@ module.exports = function () {
         },
 
         ucfirst: function (inputModel, iElem, scope, iAttrs, rulesObj) {
-            var tf = (inputModel[0].toUpperCase() === inputModel[0]);
+            var tf;
+            if (inputModel) {
+                tf = (inputModel[0].toUpperCase() === inputModel[0]);
 
-            //CORRECTOR: capitalize first letter in a string
-            if (!tf) {
-                setTimeout(function () {
-                    var newValue = inputModel.charAt(0).toUpperCase() + inputModel.slice(1);
-                    updateScope(scope, iAttrs, newValue);
-                }, 600);
+                //CORRECTOR: capitalize first letter in a string
+                if (!tf) {
+                    setTimeout(function () {
+                        var newValue = inputModel.charAt(0).toUpperCase() + inputModel.slice(1);
+                        updateScope(scope, iAttrs, newValue);
+                    }, 600);
+                }
+            } else {
+                tf = true;
             }
 
             return sendError(iElem, tf, rulesObj.ucfirst);
